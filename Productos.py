57dc159d4b076af7ea84bc0f36193157805df8b3
David Antonio Ramirez Mendez <davidrap087@gmail.com>
2024-09-22 12:17:33 -0400

    Agregando excepciones a la funcion de eliminar producto y se le agrego un bucle para seguir eliminando producto

--- Productos.py
+++ Productos.py
@@ -2,7 +2,8 @@
 import time as tp
 
 # Diccionario donde se almacenaran los productos
-Producto = {} 
+Producto = {'Coca': {'Cantidad': 23, 'Precio': 300},
+            'Jugo': {'Cantidad': 4, 'Precio': 600}} 
 
 # Funcion que hara el papel de agregar productos al diccionario
 def Agregar_Producto(Datos):
@@ -19,16 +20,26 @@ def Agregar_Producto(Datos):
     }
 
 def Eliminar_Producto():
-  Nombre = input("Introduzca el nombre del producto")
+  Nombre = input("Introduzca el nombre del producto: ")
   del Producto[Nombre]
+  print("El Producto Ha Sido Eliminado...")
+   
+  Respuesta = input("Desea Eliminar otro Producto, Si o No" )
+  try:
+    while Respuesta.lower() != "no":
+      Eliminar_Producto()
+  except:
+    print("Error Detectado")
+ 
+
 
 def Actualizar_Producto():
-  Nombre = input("Introduzca el nombre del Producto que desea Actualizar")
-  Cantidad = input("Introduzca la cantidad a modificar del producto")
+  Nombre = input("Introduzca el nombre del Producto que desea Actualizar: ")
+  Cantidad = input("Introduzca la cantidad a modificar del producto: ")
   Producto[Nombre].update({"Cantidad" : Cantidad})
 
 def Buscar_Producto():
-  Nombre = input("Introduzca el nombre del Producto")
+  Nombre = input("Introduzca el nombre del Producto: ")
   Producto[Nombre]
   
   print(f"El Producto que desea es: {Producto[Nombre]}")
@@ -50,10 +61,10 @@ while True:
   case 1:
     Agregar_Producto(Producto)
     
-    Respuesta = input("Desea Registrar Otro Producto?")
+    Respuesta = input("Desea Registrar Otro Producto? ")
     while Respuesta.lower() != "no":
       Agregar_Producto(Producto)
-      Respuesta = input("Desea Registrar Otro Producto?")
+      Respuesta = input("Desea Registrar Otro Producto ?")
 
   case 2:
    Actualizar_Producto()
@@ -61,7 +72,7 @@ while True:
    
   case 3:
      Eliminar_Producto()
-     print("Eliminando Producto")
+     
   case 4:
      print(Producto)
   case 5:
